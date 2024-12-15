1610d3958777e93da8d3ec5b605c77dddb036930
saidolim10 <git001hub@gmail.com>
2024-12-15 16:26:20 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,212 @@
+let body = document.querySelector('body')
+let header = document.querySelector('header')
+let container = document.querySelector('.container')
+let nav = document.querySelector('nav')
+let rightHead = document.querySelector('.right-head')
+let a = document.querySelector('.a')
+let a2 = document.querySelector('.a2')
+let a3 = document.querySelector('.a3')
+let ul = document.querySelector('ul')
+let btns = document.querySelector('.btns')
+let button = document.querySelector('.button')
+let button2 = document.querySelector('.button2')
+
+
+body.style.backgroundColor = '#C4B5A5'
+container.style.width = '85%'
+container.style.margin = 'auto'
+container.style.paddingTop = '30px'
+nav.style.display = 'flex'
+nav.style.justifyContent = 'space-between'
+rightHead.style.display = 'flex'
+rightHead.style.gap = '35px'
+rightHead.style.alignItems = 'center'
+ul.style.display = 'flex'
+ul.style.gap = '100px'
+ul.style.listStyle = 'none'
+a.style.textDecoration = 'none'
+a.style.color = 'black'
+a2.style.textDecoration = 'none'
+a2.style.color = 'black'
+a3.style.textDecoration = 'none'
+a3.style.color = 'black'
+a.style.fontSize = '20px'
+a2.style.fontSize = '20px'
+a3.style.fontSize = '20px'
+btns.style.display = 'flex'
+btns.style.gap = '30px'
+button.style.width = '131px'
+button.style.paddingTop = '13px'
+button.style.paddingBottom = '13px'
+button.style.borderRadius = '7px'
+button.style.border = 'none'
+button.style.fontWeight = 'bold'
+button.style.backgroundColor = '#FB982F'
+button2.style.width = '131px'
+button2.style.paddingTop = '13px'
+button2.style.paddingBottom = '13px'
+button2.style.borderRadius = '7px'
+button2.style.border = 'none'
+button2.style.fontWeight = 'bold'
+button2.style.backgroundColor = 'black'
+button2.style.color = 'white'
+
+
+
+let section = document.querySelector('section')
+let leftMain = document.querySelector('.left-main')
+let h1 = document.querySelector('h1')
+let p = document.querySelector('p')
+let btn = document.querySelector('.btn')
+
+
+section.style.display = 'flex'
+section.style.justifyContent = 'space-between'
+section.style.width = '85%'
+section.style.margin = 'auto'
+section.style.paddingTop = '100px'
+leftMain.style.width = '426px'
+leftMain.style.height = '432px'
+leftMain.style.display = 'flex'
+leftMain.style.flexDirection = 'column'
+leftMain.style.justifyContent = 'space-between'
+h1.style.fontSize = '36px'
+h1.style.width = '420px'
+body.style.fontFamily = 'poppins'
+p.style.fontSize = '16px'
+btn.style.width = '195px'
+btn.style.height = '53px'
+btn.style.borderRadius = '30px'
+btn.style.border = 'none'
+btn.style.backgroundColor = '#FA8305'
+btn.style.fontSize = '16px'
+btn.style.fontWeight = 'bold'
+
+
+
+
+function light() {
+    let body = document.querySelector('body')
+    let header = document.querySelector('header')
+    let container = document.querySelector('.container')
+    let nav = document.querySelector('nav')
+    let rightHead = document.querySelector('.right-head')
+    let a = document.querySelector('.a')
+    let a2 = document.querySelector('.a2')
+    let a3 = document.querySelector('.a3')
+    let ul = document.querySelector('ul')
+    let btns = document.querySelector('.btns')
+    let button = document.querySelector('.button')
+    let button2 = document.querySelector('.button2')
+
+    body.style.backgroundColor = '#C4B5A5'
+    container.style.width = '85%'
+    container.style.margin = 'auto'
+    container.style.paddingTop = '30px'
+    nav.style.display = 'flex'
+    nav.style.justifyContent = 'space-between'
+    rightHead.style.display = 'flex'
+    rightHead.style.gap = '35px'
+    rightHead.style.alignItems = 'center'
+    ul.style.display = 'flex'
+    ul.style.gap = '100px'
+    ul.style.listStyle = 'none'
+    a.style.textDecoration = 'none'
+    a.style.color = 'black'
+    a2.style.textDecoration = 'none'
+    a2.style.color = 'black'
+    a3.style.textDecoration = 'none'
+    a3.style.color = 'black'
+    a.style.fontSize = '20px'
+    a2.style.fontSize = '20px'
+    a3.style.fontSize = '20px'
+    btns.style.display = 'flex'
+    btns.style.gap = '30px'
+    button.style.width = '131px'
+    button.style.paddingTop = '13px'
+    button.style.paddingBottom = '13px'
+    button.style.borderRadius = '7px'
+    button.style.border = 'none'
+    button.style.fontWeight = 'bold'
+    button.style.backgroundColor = '#FB982F'
+    button2.style.width = '131px'
+    button2.style.paddingTop = '13px'
+    button2.style.paddingBottom = '13px'
+    button2.style.borderRadius = '7px'
+    button2.style.border = 'none'
+    button2.style.fontWeight = 'bold'
+    button2.style.backgroundColor = 'black'
+    button2.style.color = 'white'
+
+
+
+    let section = document.querySelector('section')
+    let leftMain = document.querySelector('.left-main')
+    let h1 = document.querySelector('h1')
+    let p = document.querySelector('p')
+    let btn = document.querySelector('.btn')
+
+
+    section.style.display = 'flex'
+    section.style.justifyContent = 'space-between'
+    section.style.width = '85%'
+    section.style.margin = 'auto'
+    section.style.paddingTop = '100px'
+    leftMain.style.width = '426px'
+    leftMain.style.height = '432px'
+    leftMain.style.display = 'flex'
+    leftMain.style.flexDirection = 'column'
+    leftMain.style.justifyContent = 'space-between'
+    h1.style.fontSize = '36px'
+    h1.style.width = '420px'
+    body.style.fontFamily = 'poppins'
+    p.style.fontSize = '16px'
+    btn.style.width = '195px'
+    btn.style.height = '53px'
+    btn.style.borderRadius = '30px'
+    btn.style.border = 'none'
+    btn.style.backgroundColor = '#FA8305'
+    btn.style.fontSize = '16px'
+    btn.style.fontWeight = 'bold'
+    body.style.color = 'black'
+    a.style.color = 'black'
+    a2.style.color = 'black'
+    a3.style.color = 'black'
+    btn.style.color = 'black'
+    button.style.color = 'black'
+
+
+}
+
+
+
+function dark() {
+    let body = document.querySelector('body');
+    body.style.backgroundColor = 'black'
+    body.style.color = 'white'
+    a.style.color = 'white'
+    a2.style.color = 'white'
+    a3.style.color = 'white'
+    btn.style.color = 'white'
+    button.style.color = 'white'
+    button2.style.color = 'white'
+    button2.style.backgroundColor = '#FA8305'
+    button.style.backgroundColor = '#C4B5A5'
+
+}
+
+function ertan() {
+    
+    let img = document.createElement('img');
+
+   
+    img.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7qHivr4xWhoZy2ZKjnzd1089kqHY6ObvLog&s";
+
+    // Optionally set an alt text for the image
+    img.alt = "Ertan Image";
+
+    
+    document.body.appendChild(img);
+    img.style.marginLeft = '700px'
+}
+
